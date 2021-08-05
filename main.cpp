0d9b42d855517e15284cb94287bdfef29171d729
Урядов Алексей <63245550+LedinecMing@users.noreply.github.com>
2021-08-05 21:22:15 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <math.h>
 #include <iostream>
+#include<Model.cpp>
 
 const float PI = 3.14;
 const float GRADUS = 0.0174;
@@ -17,13 +18,6 @@ int main()
     int wd2 = w/2;
     int hd2 = h/2;
     bool keysInfo[10] = {false, false, false, false, false, false};
-    float sinTable[180];
-    float cosTable[180]; 
-    for (int i = 0; i < 180; i++)
-    {
-        sinTable[i] = sin(i);
-        cosTable[i] = cos(i);
-    }
     sf::Vector3f camera = sf::Vector3f(-5.0, 0.0, 1.0);
     sf::Vector3f angle = sf::Vector3f(0.0, 0.0, 0.0);
     sf::RenderTexture emptyTexture;
@@ -34,16 +28,38 @@ int main()
     sf::Sprite emptySprite = sf::Sprite(emptyTexture.getTexture());
     sf::Shader shader;
     shader.loadFromFile("shader.frag", sf::Shader::Fragment);
-    shader.setUniform("uni_resolution", sf::Vector2f(w,h));
-    int objects_num = 2;
-    const float speed = 0.5;
-    float objects[objects_num*10] =
+    float speed = 0.1;
+    Model models[];
+
+    float objects[] =
     {
-        CUBE, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.0, 1.0
+        // type, x, y, z, xw, yw, zw, r, g, b
+        PLANE, 0, 0, -1,  0, 1, 1, 0, 0, 0,
+        CUBE,  0, 1,  1, 10, 60, 1, 1, 1, 1,
+        CUBE, 20, 1,  1, 10, 60, 1, 0, 0, 1,
+        CUBE, 40, 1,  1, 10, 60, 1, 1, 0, 0
     };
-    shader.setUniformArray("objects", objects, objects_num*10);
+    int objectsLength = sizeof objects / sizeof objects[0];
+
+    sf::Vector2f baseDist = sf::Vector2f(1000.0, 1000.0);
+    float lightPower = 8;
+
+    shader.setUniform("lightPower", lightPower);
+    shader.setUniform("baseDist", baseDist);
+
+    shader.setUniform("uni_resolution", sf::Vector2f(w,h));
+    shader.setUniformArray("objects", objects, objectsLength);
+
+    float sinTable[180];
+    float cosTable[180]; 
+    for (int i = 0; i < 180; i++)
+    {
+        sinTable[i] = sin(i);
+        cosTable[i] = cos(i);
+    }
     shader.setUniformArray("uni_sin", sinTable, 180);
     shader.setUniformArray("uni_cos", cosTable, 180);
+
     sf::RenderWindow window(sf::VideoMode(1600, 1200), "D3ngine", sf::Style::Fullscreen);
     window.setFramerateLimit(120);
     while (window.isOpen())
@@ -133,7 +149,7 @@ int main()
         diff = float(clock.getElapsedTime().asSeconds())-time;
         time = clock.getElapsedTime().asSeconds();
         sf::Text text;
-        text.setString(std::to_string(1/diff));
+        text.setString(std::to_string(1/diff)+" fps");
         text.setFillColor(sf::Color::Red);
         text.setCharacterSize(24);
         text.setPosition(0, 0);
@@ -142,6 +158,9 @@ int main()
         shader.setUniform("camera", camera);
         shader.setUniform("uni_angle", angle);
         window.draw(emptySprite, &shader);
+        window.draw(text);
+        text.setString("x"+std::to_string(camera.x)+"y"+std::to_string(camera.y)+"z"+std::to_string(camera.z));
+        text.setPosition(0, 24);
         window.draw(text);
         window.display();
     }
