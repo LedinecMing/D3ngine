92039cb14b0c9af882cb924080cd0fc0067bc263
Урядов Алексей <63245550+LedinecMing@users.noreply.github.com>
2021-08-31 18:09:50 +0400

    Исправлено движение

--- Camera.cpp
+++ Camera.cpp
@@ -20,8 +20,34 @@ class Camera
 
 		void move(sf::Vector3f directionToMove)
 		{
-			_position+=directionToMove*_speed;
-			// _position.x+=
+			if ( directionToMove==FORWARD )
+			{
+				this->_position.x=_position.x+(-_speed*cos( _angle.x));
+				this->_position.y=_position.y+(-_speed*sin( _angle.x));
+			}
+			else if ( directionToMove==BACKWARD )
+			{
+				this->_position.x=_position.x+(_speed*cos( _angle.x));
+				this->_position.y=_position.y+(_speed*sin( _angle.x));
+			}
+			else if ( directionToMove==LEFT )
+			{
+				this->_position.x=_position.x+(_speed*sin( _angle.x));
+				this->_position.y=_position.y+(-_speed*cos( _angle.x));	
+			}
+			else if ( directionToMove==RIGHT )
+			{
+				this->_position.x=_position.x+(-_speed*sin( _angle.x));
+				this->_position.y=_position.y+(_speed*cos( _angle.x));
+			}
+			else if ( directionToMove==UP )
+			{
+				this->_position.z+=_speed;
+			}
+			else if ( directionToMove==DOWN )
+			{
+				this->_position.z-=_speed;
+			}
 		}
 
 		void addAngle(sf::Vector3f angle)
@@ -30,8 +56,8 @@ class Camera
 		}
 
 		sf::Vector3f getPosition(){ return _position; }
-		sf::Vector3f    getAngle(){ return _angle;    }
-		sf::Vector3f     getSize(){ return _size;     }
+		sf::Vector3f getAngle(){ return _angle;}
+		sf::Vector3f getSize(){ return _size; }
 
 	private:
 		sf::Vector3f _position, _angle, _size;
